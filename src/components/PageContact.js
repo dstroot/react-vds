026f15c4b2cc80529a4a01a8ad17fb520139e00f
Dan Stroot <dan.stroot@gmail.com>
2018-07-09 19:16:04 -0700

    contact form pattern validation

--- src/components/PageContact.js
+++ src/components/PageContact.js
@@ -5,21 +5,21 @@ import React, { Component } from 'react';
 // https://daveceddia.com/ajax-requests-in-react/
 // https://blog.hellojs.org/fetching-api-data-with-react-js-460fe8bbf8f2
 
-const inputParsers = {
-  date(input) {
-    const [month, day, year] = input.split('/');
-    return `${year}-${month}-${day}`;
-  },
-  uppercase(input) {
-    return input.toUpperCase();
-  },
-  trim(input) {
-    return input.trim();
-  },
-  number(input) {
-    return parseFloat(input);
-  },
-};
+// const inputParsers = {
+//   date(input) {
+//     const [month, day, year] = input.split('/');
+//     return `${year}-${month}-${day}`;
+//   },
+//   uppercase(input) {
+//     return input.toUpperCase();
+//   },
+//   trim(input) {
+//     return input.trim();
+//   },
+//   number(input) {
+//     return parseFloat(input);
+//   },
+// };
 
 // Handle HTTP errors since fetch doesn't... :(
 // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
@@ -42,7 +42,36 @@ class PageContact extends Component {
       errorMsg: '',
       successMsg: '',
     };
+
+    this.handleTextarea = this.handleTextarea.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // https://reactjs.org/docs/forms.html
+  // Textarea does not implement pattern validation like `text` does.
+  // A "required" value passes validation with only spaces.  This
+  // adds back pattern validation.
+  handleTextarea(event) {
+    const pattern = event.target.getAttribute('pattern');
+
+    // This will enable the pattern attribute on a textarea
+    // and trigger the Html5 validation. It also takes into account
+    // patterns that have the ^ or $ operators and does a global
+    // match using the g Regex flag:
+    if (typeof pattern !== typeof undefined && pattern !== false) {
+      var patternRegex = new RegExp(
+        '^' + pattern.replace(/^\^|\$$/g, '') + '$',
+        'g'
+      );
+
+      const errorMessage = 'does not match the required pattern';
+      const input = event.target.value;
+      const hasError = !input.match(patternRegex);
+
+      if (typeof event.target.setCustomValidity === 'function') {
+        event.target.setCustomValidity(hasError ? errorMessage : '');
+      }
+    }
   }
 
   handleSubmit(event) {
@@ -63,16 +92,16 @@ class PageContact extends Component {
     const form = event.target;
     const data = new FormData(form);
 
-    // run parsers
-    for (let name of data.keys()) {
-      const input = form.elements[name];
-      const parserName = input.dataset.parse;
-      if (parserName) {
-        const parser = inputParsers[parserName];
-        const parsedValue = parser(data.get(name));
-        data.set(name, parsedValue);
-      }
-    }
+    // // run parsers
+    // for (let name of data.keys()) {
+    //   const input = form.elements[name];
+    //   const parserName = input.dataset.parse;
+    //   if (parserName) {
+    //     const parser = inputParsers[parserName];
+    //     const parsedValue = parser(data.get(name));
+    //     data.set(name, parsedValue);
+    //   }
+    // }
 
     // check form data validity
     if (!form.checkValidity()) {
@@ -148,6 +177,7 @@ class PageContact extends Component {
                   pattern=".*\S+.*"
                   data-parse="trim"
                   required
+                  autoFocus
                 />
                 <div className="invalid-feedback">Please enter your name.</div>
                 <div className="valid-feedback">Looks good!</div>
@@ -179,6 +209,7 @@ class PageContact extends Component {
                   pattern=".*\S+.*"
                   data-parse="trim"
                   required
+                  onChange={this.handleTextarea}
                 />
                 <div className="invalid-feedback">
                   Please let us know how we can help!
