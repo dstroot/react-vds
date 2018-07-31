b190ea1ac291b968ebf9e60cc3a3964f74d08486
Dan Stroot <dan.stroot@gmail.com>
2018-07-30 18:28:51 -0700

    bootstrap themeing

---
+++ src/components/HeroSection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import bg from 'startup-3.jpg';
+
+const HeroSection = () => {
+  return (
+    <div className="hero text-white" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="container">
+        <div className="row align-items-center p-2">
+          <div className="col-md-6 offset-md-6 p-lg-5 my-5">
+            <h6 className="text-uppercase bd-text-purple-bright">
+              High Praise
+            </h6>
+            <h2 className="font-weight-light">
+              “Go Analytics is amazing. Decisions that used to take weeks, now
+              only takes minutes and is available to everyone on my team.”
+            </h2>
+            <p className="font-weight-light">
+              Cindy Smith, founder of Cool Startup
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/pages/PageContact.test.js
+++ src/pages/PageContact.test.js
@@ -3,7 +3,7 @@ import PageContact from './PageContact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
-test('PageContact renders the landing page', () => {
+test('PageContact renders the contact page', () => {
   const component = renderer.create(
     <Router>
       <PageContact />
