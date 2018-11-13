1959ed66d61eb3c4116c3759fc823b311b23cf77
Dan Stroot <dan.stroot@gmail.com>
2018-11-12 20:42:06 -0800

    working

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 // NOTE: For routes to work properly in React, you need
@@ -20,7 +20,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
+serviceWorker.register();
 
 // Turn on hot module reloading, which will make it so that changes
 // you make automatically update in the app without needing to
