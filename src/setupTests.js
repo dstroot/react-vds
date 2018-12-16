3a126bcb0ef32535dc751539e9d02745c7c24a87
Dan Stroot <dan.stroot@gmail.com>
2018-12-16 08:35:29 -0800

    refactoring

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,17 +6,29 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-// Mock window.matchMedia
-// https://github.com/akiran/react-slick/issues/742
-window.matchMedia =
-  window.matchMedia ||
-  function() {
-    return {
-      matches: false,
-      addListener: function() {},
-      removeListener: function() {},
-    };
+// If some code uses a method which JSDOM (the DOM implementation used
+// by Jest) hasn't implemented yet, testing it is not easily possible. T
+// his is e.g. the case with window.matchMedia(). Jest returns TypeError:
+// window.matchMedia is not a function and doesn't properly execute the test.
+// In this case, mocking matchMedia in the test file should solve the issue:
+
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
   };
+});
+
+window.scrollTo = jest.fn().mockImplementation(() => {
+  return {
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
 
 // https://medium.com/@stipsan/testing-with-jest-15-awesome-tips-and-tricks-42150ec4c262
 // And just like that tests that have proptype warnings no longer pass!
