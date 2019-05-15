00063a977ea13f148fe0399e643c84a5785fbbb9
Dan Stroot <dan.stroot@gmail.com>
2019-05-15 08:43:52 -0700

    updated dependencies

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ Sentry.init({
   integrations: [new Sentry.Integrations.Breadcrumbs({ console: false })],
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
