b03308f26dc0e21eb6354645b76951664fda253f
Dan Stroot <dan.stroot@gmail.com>
2019-07-06 17:29:40 -0700

    testing concurrent mode

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ Sentry.init({
   integrations: [new Sentry.Integrations.Breadcrumbs({ console: false })],
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// this turns on react concurrent mode
+ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
