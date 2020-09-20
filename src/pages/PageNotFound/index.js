affa4dfce82c39e3aac4109c62c4f5a1be7d43db
Dan Stroot <dan.stroot@gmail.com>
2020-09-20 10:50:27 -0700

    updated dependencies

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+
+// routing
+import { Link, useLocation } from 'react-router-dom';
+import * as routes from '../../constants/routes';
 
 const PageNotFound = ({ image }) => {
   let location = useLocation();
@@ -16,10 +19,29 @@ const PageNotFound = ({ image }) => {
   return (
     <div className="hero" style={{ backgroundImage: `url(${image})` }}>
       <div className="d-flex justify-content-center color-wash">
-        <h1 className="mt-5 display-4 text-white">
-          Oops... We can't find:{' '}
-          <code className="text-danger">{location.pathname}</code>
-        </h1>
+        <div>
+          <h1 className="mt-5 display-4 text-white display-block">
+            Oops... We can't find:{' '}
+            <code className="text-danger">{location.pathname}</code>
+          </h1>
+          <h3 className="mt-5 display-5 text-white">
+            Try these pages instead:
+          </h3>
+          <h4 className="text-white">
+            <Link to={routes.HOME}>Home</Link>
+            {'   '}
+            <span className="text-secondary font-weight-light">
+              Ours, not yours
+            </span>
+          </h4>
+          <h4 className="text-white">
+            <Link to={routes.CONTACT}>Contact Us</Link>
+            {'   '}
+            <span className="text-secondary font-weight-light">
+              Reach out, we are pretty cool
+            </span>
+          </h4>
+        </div>
       </div>
     </div>
   );
